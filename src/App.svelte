40501620a08d463d0d91962a77072e0accd3e6a7
EinoHR <EinoHR@noreply.codeberg.org>
2022-01-09 17:21:36 +0200

    Make editor and terminal windows and content

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,91 @@
 <script>
-	export let name;
+	import * as style from './themes/horizon-dark.json';
+
+	import Colorbars from './Colorscript-Colorbars.svelte';
+	import Colorsquare from './Colorscript-Square.svelte';
+	import WindowDecorMac from './Decor-Mac.svelte';
+	import SourceCodeExample from './SourceCodeExample.svelte';
+
+	let windowStyle = 'mac';
+	
+	$: cssVarStyles = Object.entries(style["default"])
+		.map(([key, value]) => `--${key}:${value}`)
+		.join(';');
+
+	console.log(style);
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+<main style={cssVarStyles}>
+	<h1>Colorscheme Compare</h1>
+
+	<div class="terminal {windowStyle}">
+		<div class="windowdecor">
+			{#if windowStyle == 'mac'}
+				<WindowDecorMac/>
+			{/if}
+		</div><br>
+		[user@localhost] ~ # colorscript -r<br><br>
+		<Colorsquare/>
+	</div>
+
+	<div class="terminal {windowStyle}">
+		<div class="windowdecor">
+			{#if windowStyle == 'mac'}
+				<WindowDecorMac/>
+			{/if}
+		</div><br>
+		[user@localhost] ~ # colorscript -r<br><br>
+		<Colorbars/>
+	</div>
+
+	<div class="editor {windowStyle}">
+		<div class="windowdecor">
+			{#if windowStyle == 'mac'}
+				<WindowDecorMac/>
+			{/if}
+		</div>
+		<SourceCodeExample/>
+	</div>
 </main>
 
 <style>
 	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
+		font-family: "Hack Nerd Font Mono";
+		margin: 0;
+		padding: 20px;
+		box-sizing: border-box;
+		background-color: var(--ui-background);
+	}
+
+	.terminal {
+		font-family: "Hack Nerd Font Mono";
+		font-size: 17px;
+		color: var(--term-normal-white);
+		padding: 10px;
+		margin: 10px;
+	}
+
+	.editor {
+		font-family: "Hack Nerd Font Mono";
+		font-size: 14px;
+		color: var(--term-normal-white);
+		padding: 10px;
+		margin: 10px;
+	}
+
+	.mac {
+		box-shadow: #00000050 0px 10px 50px;
+		border-radius: 6px;
+		border: solid #333 1px;
+	}
+
+	:global(.windowdecorbutton) {
+		display: inline;
+		font-size: 20px;
 	}
 
 	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+		margin-top: 0;
+		color: var(--term-normal-cyan);
 	}
 </style>
