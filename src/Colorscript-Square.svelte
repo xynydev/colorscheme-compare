b571839d97fe66ec51011d138036e7de08c6eb13
EinoHR <EinoHR@noreply.codeberg.org>
2022-01-09 17:24:28 +0200

    Add two colorscripts

---
+++ src/Colorscript-Square.svelte
@@ -0,0 +1,42 @@
+<main>
+    <div class="colorblock" style="background-color: var(--term-normal-red)">&nbsp;&nbsp;&nbsp;</div><div class="colorblock" style="color: var(--term-bright-red)">▄</div>&nbsp;
+    <div class="colorblock" style="background-color: var(--term-normal-green)">&nbsp;&nbsp;&nbsp;</div><div class="colorblock" style="color: var(--term-bright-green)">▄</div>&nbsp;
+    <div class="colorblock" style="background-color: var(--term-normal-yellow)">&nbsp;&nbsp;&nbsp;</div><div class="colorblock" style="color: var(--term-bright-yellow)">▄</div>&nbsp;
+    <div class="colorblock" style="background-color: var(--term-normal-blue)">&nbsp;&nbsp;&nbsp;</div><div class="colorblock" style="color: var(--term-bright-blue)">▄</div>&nbsp;
+    <div class="colorblock" style="background-color: var(--term-normal-magenta)">&nbsp;&nbsp;&nbsp;</div><div class="colorblock" style="color: var(--term-bright-magenta)">▄</div>&nbsp;
+    <div class="colorblock" style="background-color: var(--term-normal-cyan)">&nbsp;&nbsp;&nbsp;</div><div class="colorblock" style="color: var(--term-bright-cyan)">▄</div>&nbsp;
+    <div class="colorblock" style="background-color: var(--term-normal-white)">&nbsp;&nbsp;&nbsp;</div><div class="colorblock" style="color: var(--term-bright-white)">▄</div>&nbsp;<br>
+
+    <div class="colorblock" style="background-color: var(--term-normal-red)">&nbsp;&nbsp;&nbsp;</div><div class="colorblock" style="background-color: var(--term-bright-red)">&nbsp;</div>&nbsp;
+    <div class="colorblock" style="background-color: var(--term-normal-green)">&nbsp;&nbsp;&nbsp;</div><div class="colorblock" style="background-color: var(--term-bright-green)">&nbsp;</div>&nbsp;
+    <div class="colorblock" style="background-color: var(--term-normal-yellow)">&nbsp;&nbsp;&nbsp;</div><div class="colorblock" style="background-color: var(--term-bright-yellow)">&nbsp;</div>&nbsp;
+    <div class="colorblock" style="background-color: var(--term-normal-blue)">&nbsp;&nbsp;&nbsp;</div><div class="colorblock" style="background-color: var(--term-bright-blue)">&nbsp;</div>&nbsp;
+    <div class="colorblock" style="background-color: var(--term-normal-magenta)">&nbsp;&nbsp;&nbsp;</div><div class="colorblock" style="background-color: var(--term-bright-magenta)">&nbsp;</div>&nbsp;
+    <div class="colorblock" style="background-color: var(--term-normal-cyan)">&nbsp;&nbsp;&nbsp;</div><div class="colorblock" style="background-color: var(--term-bright-cyan)">&nbsp;</div>&nbsp;
+    <div class="colorblock" style="background-color: var(--term-normal-white)">&nbsp;&nbsp;&nbsp;</div><div class="colorblock" style="background-color: var(--term-bright-white)">&nbsp;</div>&nbsp;<br>
+
+    <div class="colorblock" style="background-color: var(--term-normal-red)">&nbsp;&nbsp;&nbsp;</div><div class="colorblock" style="background-color: var(--term-bright-red)">&nbsp;</div>&nbsp;
+    <div class="colorblock" style="background-color: var(--term-normal-green)">&nbsp;&nbsp;&nbsp;</div><div class="colorblock" style="background-color: var(--term-bright-green)">&nbsp;</div>&nbsp;
+    <div class="colorblock" style="background-color: var(--term-normal-yellow)">&nbsp;&nbsp;&nbsp;</div><div class="colorblock" style="background-color: var(--term-bright-yellow)">&nbsp;</div>&nbsp;
+    <div class="colorblock" style="background-color: var(--term-normal-blue)">&nbsp;&nbsp;&nbsp;</div><div class="colorblock" style="background-color: var(--term-bright-blue)">&nbsp;</div>&nbsp;
+    <div class="colorblock" style="background-color: var(--term-normal-magenta)">&nbsp;&nbsp;&nbsp;</div><div class="colorblock" style="background-color: var(--term-bright-magenta)">&nbsp;</div>&nbsp;
+    <div class="colorblock" style="background-color: var(--term-normal-cyan)">&nbsp;&nbsp;&nbsp;</div><div class="colorblock" style="background-color: var(--term-bright-cyan)">&nbsp;</div>&nbsp;
+    <div class="colorblock" style="background-color: var(--term-normal-white)">&nbsp;&nbsp;&nbsp;</div><div class="colorblock" style="background-color: var(--term-bright-white)">&nbsp;</div>&nbsp;<br>
+
+    <div class="colorblock" style="color: var(--term-bright-red)">&nbsp;▀▀▀</div>&nbsp;
+    <div class="colorblock" style="color: var(--term-bright-green)">&nbsp;▀▀▀</div>&nbsp;
+    <div class="colorblock" style="color: var(--term-bright-yellow)">&nbsp;▀▀▀</div>&nbsp;
+    <div class="colorblock" style="color: var(--term-bright-blue)">&nbsp;▀▀▀</div>&nbsp;
+    <div class="colorblock" style="color: var(--term-bright-magenta)">&nbsp;▀▀▀</div>&nbsp;
+    <div class="colorblock" style="color: var(--term-bright-cyan)">&nbsp;▀▀▀</div>&nbsp;
+    <div class="colorblock" style="color: var(--term-bright-white)">&nbsp;▀▀▀</div>&nbsp;
+</main>
+
+<style>
+    .colorblock {
+        width:fit-content;
+        display: inline;
+        margin: 0;
+        padding: 0;
+    }
+</style>
